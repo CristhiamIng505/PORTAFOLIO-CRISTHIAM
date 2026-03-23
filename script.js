25e7f8d4af71c09c0d8b80430b415a2938c1098b
Cristhiam_Munoz <crismunozgalo77@gmail.com>
2026-03-22 20:15:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Espera a que el DOM cargue completamente
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Muestra un mensaje en consola al cargar la página
+    console.log("Portafolio cargado correctamente");
+
+});
